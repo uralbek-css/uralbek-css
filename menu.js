7ca47b397393e6048fc93cc5527c7e0806ce3616
uralbek-css <tursunovuralbek045@gmail.com>
2024-07-24 07:43:26 +0500

    update menu.js

--- menu.js
+++ menu.js
@@ -4,3 +4,21 @@ function w3_open() {
 function w3_close() {
   document.getElementById("mySidebar").style.display = "none";
 }
+
+//Mahalliy soat
+function startTime() {
+    const today = new Date();
+    let h = today.getHours();
+    let m = today.getMinutes();
+    let s = today.getSeconds();
+    h = checkTime(h);
+    m = checkTime(m);
+    s = checkTime(s);
+    
+    document.getElementById('soat').innerHTML = h + ":" + m + ":" + s;
+    setTimeout(startTime, 1000);
+}
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
