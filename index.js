21f27349f7124730db3c664ccc088b196eb12b3a
uralbek-css <tursunovuralbek045@gmail.com>
2024-10-12 23:06:53 +0500

    update index.js

--- index.js
+++ index.js
@@ -15,3 +15,11 @@ function startTime() {
   m = checkTime(m);
   s = checkTime(s);
   
+document.getElementById('soat').innerHTML =  h + ":" + m + ":" + s;
+  setTimeout(startTime, 1000);
+}
+
+function checkTime(i) {
+  if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
+  return i;
+}
