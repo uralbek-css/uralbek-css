64343ade53fcd5dd013ac5cf38c3f6f3845c2dfd
uralbek-css <tursunovuralbek045@gmail.com>
2024-10-12 23:05:38 +0500

    update index.js

--- index.js
+++ index.js
@@ -5,3 +5,13 @@ function w3_open() {
 function w3_close() {
   document.getElementById("mySidebar").style.display = "none";
 }
+
+//Clock
+function startTime() {
+  const today = new Date();
+  let h = today.getHours();
+  let m = today.getMinutes();
+  let s = today.getSeconds();
+  m = checkTime(m);
+  s = checkTime(s);
+  
